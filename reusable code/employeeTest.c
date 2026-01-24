38970ec500615ce313ded7f582b2b87a61d61794
dbackbier <137837323+dbackbier@users.noreply.github.com>
2026-01-24 18:52:32 -0500

    Delete reusable code directory

--- reusable code/employeeTest.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "Employee.h"
-
-int main(int argc, char *argv[]) {
-    Employee employee = new_Employee("John Smith", 1234);
-    printEmployee(employee);
-    printf("\n");
-}
